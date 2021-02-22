9dff41eefcf465bd593140363f21a12110ddd032
kevin_p <kprsd14@gmail.com>
2021-02-21 19:01:20 -0500

    Added media queries desktop and tablet

--- script.js
+++ script.js
@@ -4,14 +4,50 @@ const mainFooter = document.querySelector('.main-footer');
 // Share Button for active social footer
 const activeShareButton = document.querySelector('.share-icon');
 
-shareButton.forEach(button => {
-  button.addEventListener('click', () => {
-    socialFooter.classList.toggle('active');
-    mainFooter.classList.toggle('active');
-  })
-})
+// shareButton.forEach(button => {
+//   button.addEventListener('click', () => {
+//     socialFooter.classList.toggle('active');
+//     mainFooter.classList.toggle('active');
+//   })
+// })
 
-activeShareButton.addEventListener('click', () => {
-  socialFooter.classList.remove('active');
-    mainFooter.classList.remove('active');
-})
+// activeShareButton.addEventListener('click', () => {
+//   socialFooter.classList.remove('active');
+//     mainFooter.classList.remove('active');
+// })
+
+// Desktop Media Query Variable
+const mediaQuery = window.matchMedia('(min-width: 1024px)');
+// Media Query Function
+const mediaQueryChange = (e) => {
+
+  //If Desktop, else other devices
+
+  if(e.matches) {
+    shareButton.forEach(button => {
+      button.addEventListener('click', () => {
+        socialFooter.classList.toggle('active');
+        activeShareButton.remove();
+      })
+    })
+    activeShareButton.addEventListener('click', () => {
+      socialFooter.classList.remove('active');
+    })
+  } else {
+    shareButton.forEach(button => {
+      button.addEventListener('click', () => {
+        socialFooter.classList.toggle('active');
+        mainFooter.classList.toggle('active');
+      })
+    })
+    
+    activeShareButton.addEventListener('click', () => {
+      socialFooter.classList.remove('active');
+        mainFooter.classList.remove('active');
+    })
+  }
+}
+
+mediaQueryChange(mediaQuery);
+
+
